a67dfe85a2b0f8b28707a6a953c9de18a118b924
Vincent Ketteniss <v.ketteniss@outlook.com>
2022-09-13 18:47:01 +0200

    first iteration of website (cleaned bad git insertions, mb..)

--- js/templates/showcase.js
+++ js/templates/showcase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Showcase extends HTMLElement {
   constructor() {
     super();
@@ -15,21 +14,3 @@ class Showcase extends HTMLElement {
 }
 
 window.customElements.define("wc-showcase", Showcase);
-=======
-class Showcase extends HTMLElement {
-  constructor() {
-    super();
-
-    this.innerHTML = `
-    <section id="showcase">
-        <div class="container">
-            <h1>${this.getAttribute('header-text')} ${this.getAttribute('type')} </h1>
-
-        </div>
-    </section>
-`;
-  }
-}
-
-window.customElements.define("wc-showcase", Showcase);
->>>>>>> GitHub/main
